93a569b97242fdba1669cdf7bb2e0c5839bb0817
phuongok2112003 <phuong0961070156@gmail.com>
2024-05-31 21:00:29 +0700

    formdocgia

--- src/Entity/Docgia.java
+++ src/Entity/Docgia.java
@@ -7,6 +7,7 @@
 package Entity;
 
 import java.io.Serializable;
+import java.text.SimpleDateFormat;
 import java.util.Collection;
 import java.util.Date;
 import javax.persistence.Basic;
@@ -97,8 +98,9 @@ public class Docgia implements Serializable {
         this.gioiTinh = gioiTinh;
     }
 
-    public Date getNgaySinh() {
-        return ngaySinh;
+    public String getNgaySinh() {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+        return sdf.format(ngaySinh);
     }
 
     public void setNgaySinh(Date ngaySinh) {
